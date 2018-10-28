99d72a842f92b374ea47a021e5b1871698f2e256
HeShaoZe <EX-HESHAOZE001>
2018-10-28 12:10:19 +0800

    添加文件

---
+++ CoacoPodTakeAction.h
@@ -0,0 +1,22 @@
+//
+//  CoacoPodTakeAction.h
+//  CoacoPodTest
+//
+//  Created by hesz on 2018/10/28.
+//  Copyright © 2018年 HSZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CoacoPodTakeAction : NSObject
+
+- (void)coacoPodTakeOneMehtd;
+
+
+- (void)coacoPodTakeTwoMehtd;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CoacoPodTakeAction.m
@@ -0,0 +1,23 @@
+//
+//  CoacoPodTakeAction.m
+//  CoacoPodTest
+//
+//  Created by hesz on 2018/10/28.
+//  Copyright © 2018年 HSZ. All rights reserved.
+//
+
+#import "CoacoPodTakeAction.h"
+
+@implementation CoacoPodTakeAction
+
+- (void)coacoPodTakeOneMehtd
+{
+    NSLog(@"coacoPodTakeOneMehtd方法一开始行动！");
+}
+
+- (void)coacoPodTakeTwoMehtd
+{
+    NSLog(@"coacoPodTakeTwoMehtd方法二开始行动!");
+}
+
+@end
